72e68868fc49b6438cd378509b66ff7e4a1057cd
BRUT-n <brutn@list.ru>
2025-02-03 21:31:47 +0300

    task41.py

---
+++ task41.py
@@ -0,0 +1,8 @@
+from kit import show_language # импорт из кита функции
+from kit import say_hello # импорт из кита функции
+from kit import say_bye # импорт из кита функции
+
+def print_kit(): # определение функции
+    show_language() # вызов импортированной функции 
+    say_hello() # вызов импортированной функции 
+    say_bye() # вызов импортированной функции 
