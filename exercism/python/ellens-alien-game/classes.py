ee9f9d2c138e21322df4c5c8a71bbb119dde376c
BRUT-n <brutn@list.ru>
2025-04-02 22:18:46 +0300

    classes_tasks_7/7

--- exercism/python/ellens-alien-game/classes.py
+++ exercism/python/ellens-alien-game/classes.py
@@ -39,28 +39,53 @@ class Alien:
         pass
 
 
-# alien = Alien(coordinates_x=1, coordinates_y=1)
+#TODO:  create the new_aliens_collection() function below to call your Alien class with a list of coordinates.
+def new_aliens_collection(coordinates: tuple):
+    aliens_list = []
+    for x_coord, y_coord in coordinates:
+        aliens_list.append(Alien(x_coord,y_coord))
+    
+    return aliens_list
+
+
+# output tests:
+
+# task_1
+# alien = Alien(coordinates_x=2, coordinates_y=0)
+# print(alien.x_coordinate) # 2
+# print(alien.y_coordinate) # 0
+# print(alien.health) # 3
+
+# task_2
+# alien.hit()
+# print(alien.health) # 3-1 = 2
+
+# task_3
+# print(alien.is_alive())
+
+# task_4
 # alien.teleport(5, -4)
 # print(alien.x_coordinate)
 # print(alien.y_coordinate)
-# # print(alien.health)
 
-# alien.hit()
-# print(alien.health)
-
-# print(alien.is_alive())
-
-alien_one = Alien(5, 1)
-print(alien_one.total_aliens_created)
-1
-alien_two = Alien(3, 0)
-print(alien_two.total_aliens_created)
-2
-print(alien_one.total_aliens_created)
-2
-print(Alien.total_aliens_created)
+# task_6
+# alien_one = Alien(5, 1)
+# print(alien_one.total_aliens_created)
+# 1
+# alien_two = Alien(3, 0)
+# print(alien_two.total_aliens_created)
+# 2
+# print(alien_one.total_aliens_created)
+# 2
+# print(Alien.total_aliens_created)
 # Accessing the variable from the class directly
-2
+# 2
 
+# task_7
+# alien_start_positions = [(4, 7), (-1, 0)]
+# aliens = new_aliens_collection(alien_start_positions)
 
-#TODO:  create the new_aliens_collection() function below to call your Alien class with a list of coordinates.
+# for alien in aliens:
+#    	print(alien.x_coordinate, alien.y_coordinate)
+# (4, 7)
+# (-1, 0)
