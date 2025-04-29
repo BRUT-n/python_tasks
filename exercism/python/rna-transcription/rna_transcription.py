6dbf06515b5fab6186bce04231d27e94598bf233
BRUT-n <brutn@list.ru>
2025-04-29 22:48:00 +0300

    string_methods_module2

---
+++ exercism/python/rna-transcription/rna_transcription.py
@@ -0,0 +1,12 @@
+def to_rna(dna_strand):
+    dna_to_rna_converting = {"G":"C", "C":"G", "T":"A", "A":"U"} # решение через словарь
+    converting_table = dna_strand.maketrans(dna_to_rna_converting) # создание таблицы преобразования к строке на основе словаря
+    rna = dna_strand.translate(converting_table) # использование таблицы преобразования к строке
+    return rna
+
+    # решение через две строки для создания словаря
+    # dna_nucleotides = "GCTA"
+    # rna_nucleotides = "CGAU"
+    # replacing_table_for_dna = dna_strand.maketrans(dna_nucleotides, rna_nucleotides)
+    # rna_stand = dna_strand.translate(replacing_table_for_dna)
+    # return rna_stand
