78543d638822af034c8385555a54a9f16644fb94
BRUT-n <brutn@list.ru>
2025-05-14 22:52:44 +0300

    numbers_module3

--- exercism/python/armstrong-numbers/armstrong_numbers.py
+++ exercism/python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,8 @@
 def is_armstrong_number(number):
-    pass
+    string_number = str(number)
+    count = 0
+    for num in string_number:
+        num = int(num)
+        f = num ** len(string_number)
+        count += f
+    return count == number
