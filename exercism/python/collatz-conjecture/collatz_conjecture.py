392d22fe0cc7859b35550a1e508f2de770150dc8
BRUT-n <brutn@list.ru>
2025-05-19 22:14:06 +0300

    mod_commit

--- exercism/python/collatz-conjecture/collatz_conjecture.py
+++ exercism/python/collatz-conjecture/collatz_conjecture.py
@@ -3,9 +3,14 @@ def steps(number):
     if number <= 0:
         raise ValueError("Only positive integers are allowed")
     while number != 1:
-        if number % 2 == 0:
-            number = number / 2
-        else:
-            number = number * 3 + 1
+        number = number * 3 + 1 if number % 2 else number // 2 # тернарное выражение (краткий способ записи условного оператора в одну строку)
         step += 1
     return step
+
+
+    #     if number % 2 == 0:
+    #         number = number // 2 # целочисленное деление
+    #     else:
+    #         number = number * 3 + 1
+    #     step += 1
+    # return step
