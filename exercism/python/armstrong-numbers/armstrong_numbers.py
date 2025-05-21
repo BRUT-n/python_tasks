f2ce34b1008fc658f60917b91c94b0f3ff2345dd
BRUT-n <brutn@list.ru>
2025-05-22 00:01:43 +0300

    solution_2

--- exercism/python/armstrong-numbers/armstrong_numbers.py
+++ exercism/python/armstrong-numbers/armstrong_numbers.py
@@ -1,8 +1,16 @@
-def is_armstrong_number(number):
-    string_number = str(number)
-    count = 0
-    for num in string_number:
-        num = int(num)
-        f = num ** len(string_number)
-        count += f
-    return count == number
+def is_armstrong_number(number): # решение без конвертации в строки
+    number_for_calculation = number
+    result_of_iteration = 0
+    while number_for_calculation:
+        result_of_iteration += (number_for_calculation % 10) ** len(str(number))
+        number_for_calculation = number_for_calculation // 10
+    return result_of_iteration == number
+
+# def is_armstrong_number(number): # первое решение
+#     string_number = str(number)
+#     count = 0
+#     for num in string_number:
+#         num = int(num)
+#         f = num ** len(string_number)
+#         count += f
+#     return count == number
