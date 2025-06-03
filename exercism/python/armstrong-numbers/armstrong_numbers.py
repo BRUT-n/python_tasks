f18ad3b481e0258f193516e26a1661207b6adecb
BRUT-n <brutn@list.ru>
2025-06-04 00:19:32 +0300

    mod_commit

--- exercism/python/armstrong-numbers/armstrong_numbers.py
+++ exercism/python/armstrong-numbers/armstrong_numbers.py
@@ -1,8 +1,13 @@
+from math import log10, ceil
+
 def is_armstrong_number(number): # решение без конвертации в строки
+    if number == 0: # для исключения нуля в логорифме
+       return True 
     number_for_calculation = number
     result_of_iteration = 0
+    digit_count = ceil(log10(number)) #имбовая математика - ceil округление в большую сторону, floor - в нижнюю
     while number_for_calculation:
-        result_of_iteration += (number_for_calculation % 10) ** len(str(number))
+        result_of_iteration += (number_for_calculation % 10) ** digit_count
         number_for_calculation = number_for_calculation // 10
     return result_of_iteration == number
 
